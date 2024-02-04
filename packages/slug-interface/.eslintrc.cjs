4769443e76834f4610722bc67a7fb7fb8529dab8
Carlos Garrido Marín <garridomarincarlos@gmail.com>
2024-02-04 15:23:10 +0100

    feat(app): added slug interface to console

---
+++ packages/slug-interface/.eslintrc.cjs
@@ -0,0 +1,36 @@
+const { resolve } = require('node:path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    '@gisei-studio/style/eslint/node',
+    '@gisei-studio/style/eslint/typescript',
+  ].map(require.resolve),
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: [require.resolve('@gisei-studio/style/eslint/jest')],
+    },
+  ],
+  parserOptions: {
+    project,
+  },
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    'src/shims.d.ts',
+    'node_modules/',
+    'dist/',
+    '.eslintrc.js',
+  ],
+};
